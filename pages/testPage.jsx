ab77987f8e753c9d3aa2a89908df211cc039555d
HirukaKOD <hirukakodithuwakku2006@gmail.com>
2026-06-10 21:51:15 +0530

    testing 🧪

--- pages/testPage.jsx
+++ pages/testPage.jsx
@@ -1,26 +1,31 @@
+import { useState } from "react"
+import toast from "react-hot-toast"
+
 export default function TestPage(){
+    const [status, setStatus] = useState("Off")
     return(
-        <div className="w-full h-full">
-            <div className="flex flex-col items-center justify-center gap-1 w-[600px] h-[600px] bg-yellow-300">
-                <div className="w-[100px] h-[100px] bg-green-300">
-
-                </div>
-                <div className="w-[100px] h-[100px] bg-red-300">
-
-                </div>
-                <div className="w-[100px] h-[100px] bg-blue-300">
-
-                </div>
-                <div className="w-[100px] h-[100px] bg-purple-300">
-
-                </div>
-                <div className="w-[100px] h-[100px] bg-white">
-                </div>
-                
-
-
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold">{status}</h1>
+            <div className="w-90 h-12.5 flex items-center justify-center gap-4">
+                <button onClick={
+                    ()=>{
+                        setStatus("On")
+                        toast.success("Status turned On")
+                    }
+                } className="bg-blue-500 text-white px-4 py-2 rounded">turn On</button>
+                <button onClick={
+                    ()=>{
+                        setStatus("Off")
+                        toast.error("Status turned Off")
+                    }
+                } className="bg-red-500 text-white px-4 py-2 rounded">turn Off</button>
+                <button onClick={
+                    ()=>{
+                        setStatus("Idle")
+                        toast.info("Status turned Idle")
+                    }
+                } className="bg-gray-500 text-white px-4 py-2 rounded">Idle</button>
             </div>
-
         </div>
     )
 }
